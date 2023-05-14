558811f2df43847ff8841f290ea8d82638705b5b
Matheus Santos <matheussantos@devsquad.com>
2023-05-14 16:49:57 -0300

    Added `UF` enum

---
+++ src/Enums/UF.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Primitivo\DAE\Enums;
+
+enum UF: string
+{
+    case ACRE             = 'AC';
+    case ALAGOAS          = 'AL';
+    case AMAPA            = 'AP';
+    case AMAZONAS         = 'AM';
+    case BAHIA            = 'BA';
+    case CEARA            = 'CE';
+    case ESPIRITO_SANTO   = 'ES';
+    case GOIAS            = 'GO';
+    case MARANHAO         = 'MA';
+    case MATO_GROSSO      = 'MT';
+    case MATO_GROSSO_SUL  = 'MS';
+    case MINAS_GERAIS     = 'MG';
+    case PARA             = 'PA';
+    case PARAIBA          = 'PB';
+    case PARANA           = 'PR';
+    case PERNANBUCO       = 'PE';
+    case PIAUI            = 'PI';
+    case RIO_DE_JANEIRO   = 'RJ';
+    case RIO_GRANDE_NORTE = 'RN';
+    case RIO_GRANDE_SUL   = 'RS';
+    case RONDONIA         = 'RO';
+    case RORAIMA          = 'RR';
+    case SANTA_CATARINA   = 'SC';
+    case SAO_PAULO        = 'SP';
+    case SERGIPE          = 'SE';
+    case TOCANTINS        = 'TO';
+    case DISTRITO_FEDERAL = 'DF';
+}
